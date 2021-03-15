c7d0ba89097142e39530f641a399599160d80d2e
MATHEO ROBERT <matho.robert@gmail.com>
2021-03-15 19:08:54 +0100

    Added navbar link animation

--- react_portfolio/src/components/Navbar.js
+++ react_portfolio/src/components/Navbar.js
@@ -6,18 +6,29 @@ function Navbar() {
         const hamburgerPage = document.querySelector(".navbar-hamburger-menu-page")
         const hamburgerMenuBarOne = document.querySelector(".bar-one")
         const hamburgerMenuBarTwo = document.querySelector(".bar-two")
+        const navLinkHome = document.querySelector(".navbar-hamburger-menu-page-anchor-home");
+        const navLinkAbout = document.querySelector(".navbar-hamburger-menu-page-anchor-about");
+        const navLinkWork = document.querySelector(".navbar-hamburger-menu-page-anchor-work");
+        const navLinkContact = document.querySelector(".navbar-hamburger-menu-page-anchor-contact");
         hamburgerPage.classList.toggle("navbar-hamburger-menu-page-active")
         hamburgerMenuBarOne.classList.toggle("bar-one-active")
         hamburgerMenuBarTwo.classList.toggle("bar-two-active")
+        setTimeout(
+            function () {
+                navLinkHome.classList.toggle("inactive-navbar-right")
+                navLinkAbout.classList.toggle("inactive-navbar-left")
+                navLinkWork.classList.toggle("inactive-navbar-right")
+                navLinkContact.classList.toggle("inactive-navbar-left")
+            }, 650);
     }
     return (
         <React.Fragment>
             <div className="navbar-hamburger-menu-page">
                 <div className="navbar-hamburger-menu-page-anchor-wrapper">
-                    <a href="#home" aria-label="HOME anchor" name="HOME Anchor" onClick={() => handleHamburger()}>HOME</a>
-                    <a href="#about" aria-label="ABOUT anchor" name="ABOUT Anchor" onClick={() => handleHamburger()}>ABOUT</a>
-                    <a href="#work" aria-label="WORK anchor" name="WORK Anchor" onClick={() => handleHamburger()}>WORK</a>
-                    <a href="#contact" aria-label="CONTACT anchor" name="CONTACT Anchor" onClick={() => handleHamburger()}>CONTACT</a>
+                    <a className="navbar-hamburger-menu-page-anchor-home inactive-navbar-right" href="#home" aria-label="HOME anchor" name="HOME Anchor" onClick={() => handleHamburger()}>HOME</a>
+                    <a className="navbar-hamburger-menu-page-anchor-about inactive-navbar-left" href="#about" aria-label="ABOUT anchor" name="ABOUT Anchor" onClick={() => handleHamburger()}>ABOUT</a>
+                    <a className="navbar-hamburger-menu-page-anchor-work inactive-navbar-right" href="#work" aria-label="WORK anchor" name="WORK Anchor" onClick={() => handleHamburger()}>WORK</a>
+                    <a className="navbar-hamburger-menu-page-anchor-contact inactive-navbar-left" href="#contact" aria-label="CONTACT anchor" name="CONTACT Anchor" onClick={() => handleHamburger()}>CONTACT</a>
                 </div>
             </div>
             <div className="navbar-wrapper">
