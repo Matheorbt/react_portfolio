5102ce42299a22c53030b5032534c4d54c8beada
MATHEO ROBERT <matho.robert@gmail.com>
2021-03-17 18:57:24 +0100

    contact form email sending function done

--- react_portfolio/src/section/ContactSection.js
+++ react_portfolio/src/section/ContactSection.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './ContactSection.css'
+import emailjs from 'emailjs-com'
+import { init } from 'emailjs-com';
 
 function ContactSection() {
     const hideForm = () => {
@@ -9,22 +11,24 @@ function ContactSection() {
         formSentMessage.classList.toggle("contact-hidden-message");
     }
     const submitForm = () => {
+        const formContact = document.querySelector(".contact-form");
         hideForm();
-        // var templateParams = {
-        //     firstname: document.querySelector(".contact-form-input contact-form-first-name").value,
-        //     lastname: document.querySelector(".contact-form-input contact-form-last-name").value,
-        //     message: document.querySelector(".contact-form-input-message").value,
-        //     phone_number: document.querySelector(".contact-form-input contact-form-phone").value,
-        //     mail: document.querySelector(".contact-form-input contact-form-mail").value,
-        // };
+        init("user_fLHHPrdTGF5QqjiWRUaiY");
+        var templateParams = {
+            firstname: document.getElementById("form-first-name").value,
+            lastname: document.getElementById("form-last-name").value,
+            message: document.getElementById("form-message").value,
+            phone_number: document.getElementById("form-phone").value,
+            mail: document.getElementById("form-mail").value,
+        };
 
-        // window.emailjs.send("service_prfpwh6", "template_eame8yt", templateParams)
-        //     .then(function (response) {
-        //         console.log('Contact form send.', response.status, response.text);
-        //     }, function (error) {
-        //         window.alert("Erreur lors de l'envoi du formulaire veuillez reéssayer plus tard")
-        //         console.log('Contact form sending encouter issue.', error);
-        //     });
+        emailjs.send("service_prfpwh6", "template_eame8yt", templateParams, "user_fLHHPrdTGF5QqjiWRUaiY")
+            .then(function (response) {
+                console.log('Contact form send.', response.status, response.text);
+            }, function (error) {
+                window.alert("Erreur lors de l'envoi du formulaire veuillez reéssayer plus tard")
+                console.log('Contact form sending encouter issue.', error);
+            });
     }
     return (
         <React.Fragment>
@@ -39,24 +43,24 @@ function ContactSection() {
                     <div className="contact-form-top-wrapper">
                         <div className="contact-form-first-name-wrap">
                             <label>Nom</label>
-                            <input type="text" name="contact-form-input-first-name" className="contact-form-input contact-form-first-name"></input>
+                            <input id="form-first-name" type="text" name="contact-form-input-first-name" className="contact-form-input contact-form-first-name"></input>
                         </div>
                         <div className="contact-form-first-last-name-wrap">
                             <label>Prénom</label>
-                            <input type="text" name="contact-form-input-last-name" className="contact-form-input contact-form-last-name"></input>
+                            <input id="form-last-name" type="text" name="contact-form-input-last-name" className="contact-form-input contact-form-last-name"></input>
                         </div>
                         <div className="contact-form-mail-wrap">
                             <label>Mail</label>
-                            <input type="email" name="contact-form-input-mail" className="contact-form-input contact-form-mail"></input>
+                            <input id="form-mail" type="email" name="contact-form-input-mail" className="contact-form-input contact-form-mail"></input>
                         </div>
                         <div className="contact-form-phone-wrap">
                             <label>Téléphone</label>
-                            <input type="tel" name="contact-form-input-phone" className="contact-form-input contact-form-phone"></input>
+                            <input id="form-phone" type="tel" name="contact-form-input-phone" className="contact-form-input contact-form-phone"></input>
                         </div>
                     </div>
                     <div className="contact-form-bottom-wrapper">
                         <label>Message</label>
-                        <textarea type="text" name="contact-form-message" className="contact-form-input-message"></textarea>
+                        <textarea id="form-message" type="text" name="contact-form-message" className="contact-form-input-message"></textarea>
                     </div>
                     <button type="button" className="contact-form-button" onClick={() => submitForm()}><span>Envoyer</span></button>
                 </form>
