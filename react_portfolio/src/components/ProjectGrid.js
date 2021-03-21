3e116764c9c586bacb48c3d694b686dcce7a945b
MATHEO ROBERT <matho.robert@gmail.com>
2021-03-21 21:55:02 +0100

    project displaying done + react spring animation on hover (responsivity and accessibility notdone yet)

--- react_portfolio/src/components/ProjectGrid.js
+++ react_portfolio/src/components/ProjectGrid.js
@@ -1,25 +1,65 @@
 import React from 'react'
 import './ProjectGrid.css'
 import { workData } from '../data/workData'
-
+import { useSpring, animated } from 'react-spring'
+import { useState } from 'react'
 
 function ProjectGrid() {
+    //React Spring flip animation
+    const [flippedOne, setFlippedOne] = useState(false)
+    const { transformOne, opacityOne } = useSpring({
+        opacityOne: flippedOne ? 1 : 0,
+        transformOne: `perspective(600px) rotateX(${flippedOne ? 180 : 0}deg)`,
+        config: { mass: 5, tension: 500, friction: 80 }
+    })
+    const [flippedTwo, setFlippedTwo] = useState(false)
+    const { transformTwo, opacityTwo } = useSpring({
+        opacityTwo: flippedTwo ? 1 : 0,
+        transformTwo: `perspective(600px) rotateX(${flippedTwo ? 180 : 0}deg)`,
+        config: { mass: 5, tension: 500, friction: 80 }
+    })
+    const [flippedThree, setFlippedThree] = useState(false)
+    const { transformThree, opacityThree } = useSpring({
+        opacityThree: flippedThree ? 1 : 0,
+        transformThree: `perspective(600px) rotateX(${flippedThree ? 180 : 0}deg)`,
+        config: { mass: 5, tension: 500, friction: 80 }
+    })
     return (
         <React.Fragment>
+            <h1 className="work-section-hero-title">Pour en savoir plus sur un projet cliquez dessus !</h1>
             <section className="project-main-wrapper">
-                {workData.map((project) => {
-                    return (
-                        <div key={project.id} className={"work-grid-element-main-wrap number-" + project.id}>
-                            <div className="work-bottom-grid-element">
-                                <img className="work-bottom-grid-element-img" src={project.img} alt="logo preview"></img>
-                            </div>
-                            <h1 className="work-bottom-grid-element-title">{project.title}</h1>
-                            <p className="work-bottom-grid-element-description">{project.description}</p>
-                        </div>
-                    )
-                })}
+                <div className={"work-grid-element-main-wrap number-" + workData[0].id} onClick={() => setFlippedOne(state => !state)}>
+                    <animated.div className="work-bottom-grid-element-front" style={{ opacity: opacityOne.interpolate(o => 1 - o), transform: transformOne }}>
+                        <img className="work-bottom-grid-element-img" src={workData[0].img} alt="logo preview"></img>
+                    </animated.div>
+                    <animated.div className="work-bottom-grid-element-back" style={{ opacity: opacityOne, transform: transformOne.interpolate(t => `${t} rotateX(180deg)`) }} >
+                        <h1 className="work-bottom-grid-element-title">{workData[0].title}</h1>
+                        <p className="work-bottom-grid-element-description">{workData[0].description}</p>
+                    </animated.div>
+                </div>
+                <a className="work-bottom-grid-element-link" href={workData[0].projectLink}>Accéder au projet !</a>
+                <div className={"work-grid-element-main-wrap number-" + workData[1].id} onClick={() => setFlippedTwo(state => !state)}>
+                    <animated.div className="work-bottom-grid-element-front" style={{ opacity: opacityTwo.interpolate(o => 1 - o), transform: transformTwo }}>
+                        <img className="work-bottom-grid-element-img" src={workData[1].img} alt="logo preview"></img>
+                    </animated.div>
+                    <animated.div className="work-bottom-grid-element-back" style={{ opacity: opacityTwo, transform: transformTwo.interpolate(t => `${t} rotateX(180deg)`) }} >
+                        <h1 className="work-bottom-grid-element-title">{workData[1].title}</h1>
+                        <p className="work-bottom-grid-element-description">{workData[1].description}</p>
+                    </animated.div>
+                </div>
+                <a className="work-bottom-grid-element-link" href={workData[1].projectLink}>Accéder au projet !</a>
+                <div className={"work-grid-element-main-wrap number-" + workData[2].id} onClick={() => setFlippedThree(state => !state)}>
+                    <animated.div className="work-bottom-grid-element-front" style={{ opacity: opacityThree.interpolate(o => 1 - o), transform: transformThree }}>
+                        <img className="work-bottom-grid-element-img" src={workData[2].img} alt="logo preview"></img>
+                    </animated.div>
+                    <animated.div className="work-bottom-grid-element-back" style={{ opacity: opacityThree, transform: transformThree.interpolate(t => `${t} rotateX(180deg)`) }} >
+                        <h1 className="work-bottom-grid-element-title">{workData[2].title}</h1>
+                        <p className="work-bottom-grid-element-description">{workData[2].description}</p>
+                    </animated.div>
+                </div>
+                <a className="work-bottom-grid-element-link" target="_blank" href={workData[2].projectLink}>Accéder au projet !</a>
             </section>
-        </React.Fragment>
+        </React.Fragment >
     )
 }
 
--- react_portfolio/src/data/workData.js
+++ react_portfolio/src/data/workData.js
@@ -3,18 +3,21 @@ export const workData = [
         id: 1,
         img: 'https://user-images.githubusercontent.com/57138558/110000429-9013cf00-7d13-11eb-97c9-a3f5dc9c3ec4.PNG',
         title: 'Portfolio using Parallax',
-        description: 'lorem ipsum'
+        description: 'Site inspiré du portfolio de Kuon Yagi utilisant la librairie fullPage.js, parallax.js, wow.js et le préprocesseur SASS',
+        projectLink: ''
     },
     {
         id: 2,
         img: 'https://user-images.githubusercontent.com/57138558/110000559-ba658c80-7d13-11eb-9c53-83dd00fefa4d.PNG',
         title: 'React Bookshelf',
-        description: 'lorem ipsum'
+        description: 'Projet réalisé via le framework React afin d\'utiliser les hook comme useState',
+        projectLink: ''
     },
     {
         id: 3,
         img: 'https://user-images.githubusercontent.com/57138558/107234775-4b528c00-6a24-11eb-9f76-93ee9ad5f331.PNG',
         title: 'JS Calculator',
-        description: 'lorem ipsum'
+        description: 'Simple calculatrice réalisée avec HTML, CSS, Javascript',
+        projectLink: 'https://css-js-calculator.netlify.app/'
     },
 ];
